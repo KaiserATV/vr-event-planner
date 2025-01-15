68500a00cc7fa75398462341823f5b9acd9e9c1a
Julia-Robin Schuster <juroschu@googlemail.com>
2025-01-15 21:54:11 +0100

    skript weitergemacht, aber noch nicht funktionell

--- Gluehweinplaner/Assets/RadialSelection.cs
+++ Gluehweinplaner/Assets/RadialSelection.cs
@@ -2,11 +2,13 @@ using System.Collections.Generic;
 using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Events;
 using System;
 
 
 public class RadicalSelection : MonoBehaviour
 {
+    //public OVRInput.Button spawnButton;
     [Range(2, 10)]
     public int numberOfRadialPart;
     public GameObject radialPartPrefab;
@@ -22,14 +24,33 @@ public class RadicalSelection : MonoBehaviour
 
     void Start()
     {
+        Debug.Log($"RadialPartCanvas Active: {radialPartCanvas.gameObject.activeSelf}");
+        Debug.Log($"Hand Position: {handTransform.position}, Rotation: {handTransform.rotation}");
+        Debug.Log($"Current Selected Part: {currentSelectedRadialPart}");
 
     }
 
     void Update()
     {
+        /*if(OVRInput.GetDown(spawnButton))
+        {
+            SpawnRadialPart();
+        }
+        if(OVRInput.Get(spawnButton))
+        {
+            GetSelectedRadialPart();
+        }
+        if(OVRInput.GetUp(spawnButton))
+        {
+            HideAndTriggerSelected();
+        }*/
         SpawnRadialPart();
         GetSelectedRadialPart();
         HideAndTriggerSelected();
+        Debug.Log($"RadialPartCanvas Active: {radialPartCanvas.gameObject.activeSelf}");
+        Debug.Log($"Hand Position: {handTransform.position}, Rotation: {handTransform.rotation}");
+        Debug.Log($"Current Selected Part: {currentSelectedRadialPart}");
+
     }
 
     private void HideAndTriggerSelected()
@@ -74,19 +95,24 @@ public class RadicalSelection : MonoBehaviour
         radialPartCanvas.gameObject.SetActive(true);
         radialPartCanvas.position = handTransform.position;
         radialPartCanvas.rotation = handTransform.rotation;
+
         foreach (var item in spawnedParts)
         {
             Destroy(item);
         }
+
         spawnedParts.Clear();
+
         for (int i = 0; i < numberOfRadialPart; i++)
         {
             float angle = -i * 360 / numberOfRadialPart - angleBetweenPart / 2;
             Vector3 radialPartEulerAngle = new Vector3(0, 0, angle);
+
             GameObject spawnedRadialPart = Instantiate(radialPartPrefab, radialPartCanvas);
             spawnedRadialPart.transform.position = radialPartCanvas.position;
             spawnedRadialPart.transform.localEulerAngles = radialPartEulerAngle;
-            spawnedRadialPart.GetComponent<Image>().fillAmount = (1 / (float)numberOfRadialPart - (angleBetweenPart / 360));
+
+            spawnedRadialPart.GetComponent<Image>().fillAmount = (1 / (float)numberOfRadialPart) - (angleBetweenPart / 360);
             spawnedParts.Add(spawnedRadialPart);
         }
     }
